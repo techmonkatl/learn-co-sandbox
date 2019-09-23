b12f0932fd573575eec9c7284341f4a1afeebac2
Bronze Law <bronzelaw3@gmail.com>
2019-09-23 20:44:06 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,6 @@
+def greeting 
+  puts "Hi, Ruby programmer!"
+end
+
+greeting
+#=> Hi, Ruby programmer
---
+++ main.rb
@@ -0,0 +1,18 @@
+def greet(name)
+  puts "hello, #{name[0]}"
+  puts "hello, #{name[1]}"
+  puts "hello, #{name[2]}"
+  puts "hello, #{name[3]}"
+end
+
+
+greet (["carrington", "bronze", "javi", "mauricio"])
+
+
+
+
+
+
+
+
+
